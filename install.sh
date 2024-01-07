94dad423581879167ee4bb5e46fb3f5ce2a1d114
Ta Gia Vinh <gvinhh@gmail.com>
2024-01-07 09:07:31 +0700

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TERMINAL_PACKAGES="git curl tree snapd vim zsh gnome-shell-extension-manager "
+TERMINAL_PACKAGES="git curl tree snapd vim zsh gnome-shell-extension-manager python3-pip"
 PIP_PACKAGES="gnome-extensions-cli"
 ZSH_PLUGINS="https://github.com/zsh-users/zsh-syntax-highlighting https://github.com/zsh-users/zsh-autosuggestions https://github.com/marlonrichert/zsh-autocomplete.git"
 VIM_PLUGINS="https://tpope.io/vim/surround.git"
