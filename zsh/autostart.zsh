9044bd2a0c488baf3809606f74e885a53a031b22
Colpno <gvinhh@gmail.com>
2024-03-19 02:07:29 +0700

    fixed todo file doesn't exist due to wrong variable reference

--- zsh/autostart.zsh
+++ zsh/autostart.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 TODO_FILE="$HOME/.todo/todo.txt"
 
-if [ -s TODO_FILE ]; then
-	todo.sh ls
+if [ -e $TODO_FILE ]; then
+	if [ -s $TODO_FILE ]; then
+		   todo.sh ls
+	fi
+else
+	echo "$TODO_FILE doesn't exist"
 fi
