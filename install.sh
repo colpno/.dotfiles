574752d152e2069d153dddddda0f149abbf7525d
Colpno <gvinhh@gmail.com>
2024-08-16 16:36:30 +0700

    implement try catch

--- install.sh
+++ install.sh
@@ -96,43 +96,62 @@ install_fonts() {
 
 	local FONT_DIR="/usr/share/fonts"
 
-	sudo find "$DOTFILES/fonts" -name "*.[ot]tf" -type f -exec cp -v {} "$FONT_DIR/" \;
+	{
+		sudo find "$DOTFILES/fonts" -name "*.[ot]tf" -type f -exec cp -v {} "$FONT_DIR/" \;
 
-	if which fc-cache >/dev/null 2>&1 ;
-	then
-		fc-cache -fv "$USER_FONT_DIR"
-		source /etc/profile
-	fi
+		if which fc-cache >/dev/null 2>&1 ;
+		then
+			fc-cache -fv "$USER_FONT_DIR"
+			source /etc/profile
+		fi
 
-	success "Fonts are installed"
+		success "Fonts are installed"
+	} || {
+		error "Failed to install fonts"
+	}
+
 }
 
 setup_terminal_profile() {
 	title "Configuring profile"
 
 	info "Installing oh-my-zsh"
-	sh -cv "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-	success "oh-my-zsh is installed"
+	{
+		sh -cv "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+		success "oh-my-zsh is installed"
+	} || {
+		error "Failed to install oh-my-zsh"
+	}
 
 	info "Installing oh-my-zsh's plugins"
-	cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/plugins}
-	for plugin in $ZSH_PLUGINS
-	do
-		info "Installing $plugin"
-		git clone --depth=1 $plugin
-		success "$plugin is installed"
-	done
+	{
+		cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/plugins}
+		for plugin in $ZSH_PLUGINS
+		do
+			info "Installing $plugin"
+			git clone --depth=1 $plugin
+			success "$plugin is installed"
+		done
+	} || {
+		error "Failed to install oh-my-zsh plugins"
+	}
 	cd $DOTFILES
 
-	info "Installing powerlevel10k theme"
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/themes/powerlevel10k}
-	success "powerlevel10k is installed"
+	info "Installing Powerlevel10k"
+	{
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/themes/powerlevel10k}
+		success "Powerlevel10k is installed"
+	} || {
+		error "Failed to install Powerlevel10k"
+	}
 
 	info "Restoring user dconf"
-	dconf load / < ${DOTFILES}/gnome-terminal/user.dconf
-	success "User dconf is restored"
-
-	success "Profile is configured"
+	{
+		dconf load / < ${DOTFILES}/gnome-terminal/user.dconf
+		success "User dconf is restored"
+	} || {
+		error "Failed to restore user dconf"
+	}
 }
 
 create_symlinks() {
@@ -140,17 +159,26 @@ create_symlinks() {
 
 	for path in $SYMLINKS_HOME
 	do
-		local filename=$(basename "$path")
-		info "Creating symlink for $filename"
-		rm -rfv "$filename"
-		ln -svf "$DOTFILES/$path" ~/."$filename"
-		success "Symlink of $filename is created"
+		{
+			local filename=$(basename "$path")
+			info "Creating symlink for $filename"
+			rm -rfv "$filename"
+			ln -svf "$DOTFILES/$path" ~/."$filename"
+			success "Symlink of $filename is created"
+		} || {
+			error "Failed to create symlink for $filename"
+		}
 	done
 
-	rm -rfv ~/.todo
-	create_dir_if_not_exist ~/.todo
-	ln -svf "$DOTFILES/todotxt/config" ~/.todo/config
-	success "Symlink of todo config is created"
+	{
+		info "Creating symlink for todotxt config"
+		rm -rfv ~/.todo
+		create_dir_if_not_exist ~/.todo
+		ln -svf "$DOTFILES/todotxt/config" ~/.todo/config
+		success "Symlink of todo config is created"
+	} || {
+		error "Failed to create symlink for todotxt config"
+	}
 }
 
 install_js_pkg_managers() {
@@ -159,49 +187,73 @@ install_js_pkg_managers() {
 	local packages=("$@")
 
 	if [[ "${packages[@]}" =~ "npm" ]]; then
-		info "Installing nvm"
-		curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-		source ~/.zshrc
-		success "nvm is installed"
+		info "Installing NVM"
+		{
+			curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+			source ~/.zshrc
+			success "NVM is installed"
+		} || {
+			error "Failed to install NVM"
+		}
 
-		info "Installing npm"
-		nvm install --lts
-		success "Latest npm version is installed"
+		info "Installing NPM"
+		{
+			nvm install --lts
+			success "Latest NPM version is installed"
+		} || {
+			error "Failed to install NPM"
+		}
 	fi
 
 	if [[ "${packages[@]}" =~ "yarn" ]]; then
-		info "Installing yarn"
-		curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-		echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-		sudo apt update
-		sudo apt install --no-install-recommends yarn
-		success "Latest yarn version is installed"
+		info "Installing Yarn"
+		{
+			curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+			echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+			sudo apt update
+			sudo apt install --no-install-recommends yarn
+			success "Latest Yarn version is installed"
+		} || {
+			error "Failed to install Yarn"
+		}
 	fi
 }
 
 setup_github_ssh() {
-	title "Generating github ssh key"
+	title "Generating Github SSH key"
 
-	ssh-keygen -t ed25519 -C "gvinhh@gmail.com"
-	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_ed25519
+	{
+		ssh-keygen -t ed25519 -C "gvinhh@gmail.com"
+		eval "$(ssh-agent -s)"
+		ssh-add ~/.ssh/id_ed25519
 
-	info "SSH key: "
-	cat ~/.ssh/id_ed25519.pub
-	read -p "ENTER when done Github SSH establishing"
+		info "SSH key: "
+		cat ~/.ssh/id_ed25519.pub
+		read -p "ENTER when done Github SSH establishing"
+	} || {
+		error "Failed to generate Github SSH key"
+	}
 }
 
 install_gnome_extensions() {
 	title "Installing gnome extensions"
 
-	pipx install gnome-extensions-cli
+	{
+		pipx install gnome-extensions-cli
+		for extension in $GNOME_EXTENSIONS
+		do
+			{
+				info "Installing $extension"
+				gext install $extension
+				success "$extension is installed"
+			} || {
+				error "Failed to install $extension"
+			}
+		done
+	} || {
+		error "Failed to install gnome-extensions-cli"
+	}
 
-	for extension in $GNOME_EXTENSIONS
-	do
-		info "Installing $extension"
-		gext install $extension
-		success "$extension is installed"
-	done
 }
 
 install_programs() {
@@ -212,43 +264,59 @@ install_programs() {
 	if [[ "${programs[@]}" =~ "VS Code" ]]; then
 		info "Installing VS Code"
 
-		wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-		sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-		sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-		rm -f packages.microsoft.gpg
-		sudo apt update
-		sudo apt install code
+		{
+			wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+			sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+			sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+			rm -f packages.microsoft.gpg
+			sudo apt update
+			sudo apt install code
 
-		success "VS Code is installed"
+			success "VS Code is installed"
+		} || {
+			error "Failed to install VS Code"
+		}
 	fi
 
 	if [[ "${programs[@]}" =~ "OBS Studio" ]]; then
 		info "Installing OBS Studio"
 
-		sudo add-apt-repository ppa:obsproject/obs-studio
-		sudo apt install -y obs-studio
+		{
+			sudo add-apt-repository ppa:obsproject/obs-studio
+			sudo apt install -y obs-studio
 
-		success "OBS Studio is installed"
+			success "OBS Studio is installed"
+		} || {
+			error "Failed to install OBS Studio"
+		}
 	fi
 
 	if [[ "${programs[@]}" =~ "Postman" ]]; then
 		info "Installing Postman"
 
-		sudo snap install postman
-
-		success "Postman is installed"
+		{
+			sudo snap install postman
+			success "Postman is installed"
+		} || {
+			error "Failed to install Postman"
+		}
 	fi
 
 	if [[ "${programs[@]}" =~ "Spotify" ]]; then
 		info "Installing Spotify"
 
-		git clone --depth=1 https://github.com/abba23/spotify-adblock.git
-		if [ $? -eq 0 ]; then
-			cd spotify-adblock && make && sudo make install && cd ../ && rm -rf spotify-adblock
-		else
-			cd archive/spotify-adblock && make && sudo make install && cd "$DOTFILES"
+		{
+			git clone --depth=1 https://github.com/abba23/spotify-adblock.git
+			if [ $? -eq 0 ]; then
+				cd spotify-adblock && make && sudo make install && cd ../ && rm -rf spotify-adblock
+			else
+				cd archive/spotify-adblock && make && sudo make install && cd "$DOTFILES"
+			fi
 
-		success "Spotify is installed"
+			success "Spotify is installed"
+		} || {
+			error "Failed to install Spotify"
+		}
 	fi
 }
 
@@ -257,36 +325,56 @@ install_apt_pkg() {
 
 	for package in $APT_PACKAGES
 	do
-		info "Installing $package"
-		sudo apt install -y "$package"
-		success  "$package is installed"
+		{
+			info "Installing $package"
+			sudo apt install -y "$package"
+			success  "$package is installed"
+		} || {
+			error "Failed to install $package"
+		}
 	done
 
-	title "Installing todo.txt"
-	local DIR="$HOME/.todo"
-	local GIT_FOLDER="$DIR/git-folder"
-	create_dir_if_not_exist "$DIR"
-	create_dir_if_not_exist "$GIT_FOLDER"
-	git clone --depth=1 git@github.com:todotxt/todo.txt-cli.git "$GIT_FOLDER"
-	cd "$GIT_FOLDER" && make && sudo make install CONFIG_DIR=$DIR
-	rm -rfv "$GIT_FOLDER"
-	success "todo.txt is installed"
+	title "Installing todotxt"
+	{
+		local DIR="$HOME/.todo"
+		local GIT_FOLDER="$DIR/git-folder"
+		create_dir_if_not_exist "$DIR"
+		create_dir_if_not_exist "$GIT_FOLDER"
+		git clone --depth=1 git@github.com:todotxt/todo.txt-cli.git "$GIT_FOLDER"
+		cd "$GIT_FOLDER" && make && sudo make install CONFIG_DIR=$DIR
+		rm -rfv "$GIT_FOLDER"
+		success "todotxt is installed"
+	} || {
+		error "Failed to install todotxt"
+	}
 }
 
 install_laravel() {
 	title "Installing Laravel"
 
 	info "Installing PHP"
-	sudo apt install -y php php-common php-cli php-gd php-mysqlnd php-curl php-intl php-mbstring php-bcmath php-xml php-zip
-	success "PHP is installed"
+	{
+		sudo apt install -y php php-common php-cli php-gd php-mysqlnd php-curl php-intl php-mbstring php-bcmath php-xml php-zip
+		success "PHP is installed"
+	} || {
+		error "Failed to install PHP"
+	}
 
 	info "Installing Composer"
-	curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-	success "Composer is installed"
+	{
+		curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+		success "Composer is installed"
+	} || {
+		error "Failed to install Composer"
+	}
 
 	info "Installing MySQL"
-	sudo apt install -y mysql-server
-	success "MySQL is installed"
+	{
+		sudo apt install -y mysql-server
+		success "MySQL is installed"
+	} || {
+		error "Failed to install MySQL"
+	}
 }
 
 clean_up() {
@@ -295,7 +383,11 @@ clean_up() {
 	for package in $CLEAN_UP_APT_PACKAGES
 	do
 		info "Uninstalling $package"
-		sudo apt remove --purge -y "$package"
+		{
+			sudo apt remove --purge -y "$package"
+		} || {
+			error "Failed to uninstall $package"
+		}
 	done
 
 	sudo apt autoremove -y
