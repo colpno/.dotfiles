01f14540e834c38a7b4dd33557c69cf2f2234d54
Colpno <gvinhh@gmail.com>
2025-02-17 20:03:43 +0700

    remove function.zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,9 +1,2 @@
-source ~/.dotfiles/zsh/function.zsh
-
-alias run=run_project
-alias updatecode='sudo apt update && sudo apt install code'
-alias mongo=mongo_runner
 alias t=todo.sh
 alias c=code
-alias createlaravel="composer create-project laravel/laravel $1"
-alias runlaravel='php artisan serve'
